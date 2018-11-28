bce2b0f47531c2105e131264c11ef2b2cf268186
gabchak <andrey.gabchak@gmail.com>
2018-11-28 17:01:42 +0200

    Just one 'for' cycle.

--- src/main/java/com/gabchak/Main.java
+++ src/main/java/com/gabchak/Main.java
@@ -2,33 +2,15 @@ package com.gabchak;
 
 public class Main {
 
-    private int[] fillArray(int begining, int end) {
-        int[] array = new int[end - begining];
-        for (int i = begining, j = 0; i <= end; i++, j++) {
-            array[j] = i;
-        }
-        return array;
-    }
-
     public static void main(String[] args) {
-        Main main = new Main();
-        int[] inputArray = main.fillArray(1000, 9999);
-
-        int counter = main.countQuantityOf2PNumbers(inputArray);
-
-        System.out.println(counter);
-    }
-
-    private int countQuantityOf2PNumbers(int[] inputArray) {
-
         int counter = 0;
-
-        for (int elem : inputArray) {
-            if ((elem / 1000 + (elem / 100) % 10) == ((elem / 10) % 10 + elem % 10)) {
+        for (int i = 1000; i <= 9999; i++) {
+            if ((i / 1000 + (i / 100) % 10) == ((i / 10) % 10 + i % 10)) {
                 counter++;
             }
         }
-        return counter;
+
+        System.out.println(counter);
     }
 
 }
